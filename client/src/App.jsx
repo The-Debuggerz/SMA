7358c85f1e08a863b4fe008042b781cbac82eb0b
The_flaSK <theflask@localhost.localdomain>
2022-10-05 19:38:07 +0530

    fetch posts from server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-import HomePage from './components/HomePage/HomePage';
-import './App.css';
+import HomePage from "./components/HomePage/HomePage";
+import "./App.css";
 
 function App() {
   return (
