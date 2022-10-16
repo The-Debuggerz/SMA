5a6bf42a38d06fc3e54eb51fc932f25d0234d760
BhavyaCodes <xbhavyatomar@gmail.com>
2022-10-16 19:41:54 +0530

    serve client build from server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
-const fs = require('fs');
+// const fs = require('fs');
 const path = require('path');
-const https = require('https');
+const http = require('http');
 
 const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 
-https
+http
   .createServer(
-    {
-      key: fs.readFileSync(path.join(__dirname, 'certificates', 'key.pem')),
-      cert: fs.readFileSync(path.join(__dirname, 'certificates', 'cert.pem')),
-    },
+    // {
+    //   key: fs.readFileSync(path.join(__dirname, 'certificates', 'key.pem')),
+    //   cert: fs.readFileSync(path.join(__dirname, 'certificates', 'cert.pem')),
+    // },
     app
   )
   .listen(PORT, () => {
