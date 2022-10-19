71a962e23000fca67b66127e0975804a95f23f56
deep_ramani <41836849+deepramani8505@users.noreply.github.com>
2022-10-19 22:00:43 +0530

    Prettier issue Fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,30 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import Navbar from "./components/Navbar/Navbar";
-import HomePage from "./components/HomePage/HomePage";
-import RegisterPage from "./components/RegisterPage/RegisterPage";
-import Profile from "./components/Profile/Profile";
-import AboutPage from "./components/AboutPage/AboutPage";
-import Login from "./components/LoginPage/Login";
-import AdminDashBoard from "./components/AdminDashboard/AdminDashBoard";
-import ChatPage from "./components/ChatPage/ChatPage";
+import Navbar from './components/Navbar/Navbar';
+import HomePage from './components/HomePage/HomePage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
+import Profile from './components/Profile/Profile';
+import AboutPage from './components/AboutPage/AboutPage';
+import Login from './components/LoginPage/Login';
+import AdminDashBoard from './components/AdminDashboard/AdminDashBoard';
+import ChatPage from './components/ChatPage/ChatPage';
 
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Navbar />}>
+        <Route path='/' element={<Navbar />}>
           <Route index element={<HomePage />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<RegisterPage />} />
+          <Route path='login' element={<Login />} />
+          <Route path='signup' element={<RegisterPage />} />
           {/* <Route path='posts' element={< />} /> */}
-          <Route path="about" element={<AboutPage />} />
-          <Route path="admin" element={<AdminDashBoard />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="chatPage" element={<ChatPage />} />
+          <Route path='about' element={<AboutPage />} />
+          <Route path='admin' element={<AdminDashBoard />} />
+          <Route path='profile' element={<Profile />} />
+          <Route path='chatPage' element={<ChatPage />} />
         </Route>
       </Routes>
     </>
