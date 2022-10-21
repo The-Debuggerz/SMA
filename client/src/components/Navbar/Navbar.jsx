911964fa695dde4cf9da84e212c506290474b10b
Deep-Ramani <deepramani8505@gmail.com>
2022-10-21 09:41:15 +0530

    Create Settings Page

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -131,7 +131,7 @@ export default function Navbar() {
                         <Menu.Item>
                           {({ active }) => (
                             <a
-                              href='#'
+                              href='/settings'
                               className={classNames(
                                 active ? 'bg-gray-100' : '',
                                 'block px-4 py-2 text-sm text-gray-700'
