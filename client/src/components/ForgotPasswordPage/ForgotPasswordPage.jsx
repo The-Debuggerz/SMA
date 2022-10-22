6c769a73266663647276fa33e9e74bd4d359a5ea
J4WEB <prakharsrivastava841@gmail.com>
2022-10-23 03:39:18 +0530

    footer placed

--- client/src/components/ForgotPasswordPage/ForgotPasswordPage.jsx
+++ client/src/components/ForgotPasswordPage/ForgotPasswordPage.jsx
@@ -3,23 +3,23 @@ import logo from '../Logo/logo.png';
 const ForgotPasswordPage = () => {
   return (
     <div>
-      <section class=''>
-        <div class='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
+      <section className=''>
+        <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
           <a
             href='#'
-            class='flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white'
+            className='flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white'
           >
-            <img class='w-auto h-12 mr-2' src={logo} alt='logo' />
+            <img className='w-auto h-12 mr-2' src={logo} alt='logo' />
           </a>
-          <div class='w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8'>
-            <h2 class='mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
+          <div className='w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8'>
+            <h2 className='mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
               Change Password
             </h2>
-            <form class='mt-4 space-y-4 lg:mt-5 md:space-y-5' action='#'>
+            <form className='mt-4 space-y-4 lg:mt-5 md:space-y-5' action='#'>
               <div>
                 <label
-                  for='email'
-                  class='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+                  Htmlfor='email'
+                  className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
                 >
                   Your email
                 </label>
@@ -34,7 +34,7 @@ const ForgotPasswordPage = () => {
               </div>
               <div>
                 <label
-                  for='password'
+                  Htmlfor='password'
                   class='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
                 >
                   New Password
@@ -50,7 +50,7 @@ const ForgotPasswordPage = () => {
               </div>
               <div>
                 <label
-                  for='confirm-password'
+                  Htmlfor='confirm-password'
                   class='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
                 >
                   Confirm password
@@ -60,12 +60,12 @@ const ForgotPasswordPage = () => {
                   name='confirm-password'
                   id='confirm-password'
                   placeholder='••••••••'
-                  class='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                   required=''
                 />
               </div>
-              <div class='flex items-start'>
-                <div class='flex items-center h-5'>
+              <div className='flex items-start'>
+                <div className='flex items-center h-5'>
                   <input
                     id='newsletter'
                     aria-describedby='newsletter'
@@ -76,12 +76,12 @@ const ForgotPasswordPage = () => {
                 </div>
                 <div class='ml-3 text-sm'>
                   <label
-                    for='newsletter'
-                    class='font-light text-gray-500 dark:text-gray-300'
+                    Htmlfor='newsletter'
+                    className='font-light text-gray-500 dark:text-gray-300'
                   >
                     I accept the{' '}
                     <a
-                      class='font-medium text-primary-600 hover:underline dark:text-primary-500'
+                      className='font-medium text-primary-600 hover:underline dark:text-primary-500'
                       href='#'
                     >
                       Terms and Conditions
@@ -91,7 +91,7 @@ const ForgotPasswordPage = () => {
               </div>
               <button
                 type='submit'
-                class='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
+                className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
               >
                 Reset passwod
               </button>
