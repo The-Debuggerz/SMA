f89554ac9c0eee6188a83a4513591b8a912b7ea6
Mujib Sayyad <63368252+mujibsayyad@users.noreply.github.com>
2022-11-14 13:18:31 +0530

    Update index.js

--- client/src/util/index.js
+++ client/src/util/index.js
@@ -2,8 +2,8 @@ export function getGoogleOAuthURL() {
   const rootUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
 
   const options = {
-    redirect_uri: import.meta.env.VITE_GOOGLE_OAUTH_REDIRECT_URL,
-    client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+    redirect_uri: VITE_GOOGLE_OAUTH_REDIRECT_URL,
+    client_id: VITE_GOOGLE_CLIENT_ID,
     access_type: 'offline',
     response_type: 'code',
     prompt: 'consent',
